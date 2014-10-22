dba264b53db6d0315354b4564541ecea6f259b63
Gab-Metzger <gab.metzger@gmail.com>
2014-10-22 22:11:17 +0200

    First commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('timerDocFullstackApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
