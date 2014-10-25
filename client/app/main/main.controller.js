719eca883d3aea52e008d91f18a78abdb2c0f2c6
Gab-Metzger <gab.metzger@gmail.com>
2014-10-25 12:03:57 +0200

    Add markers on map && display doctor infos on click

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,21 @@
 'use strict';
 
 angular.module('timerDocFullstackApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
+  .controller('MainCtrl', function ($scope, $http, socket, Doctor) {
+
+    Doctor.get().success(function (data,status) {
+        $scope.doctors = data;
+        socket.syncUpdates('doctor', $scope.doctors);
+    });
+
+    $scope.chooseLabelColor = function(doctor) {
+        if ((doctor.nbPatient * doctor.averageTime) >= 60) {
+            return 'marker-labels-red';
+        }
+        else {
+            return 'marker-labels-green';
+        }
+    };
 
     $scope.map = {
         center: {
@@ -11,5 +25,13 @@ angular.module('timerDocFullstackApp')
         zoom: 14
     };
 
-    //$scope.searchbox = { template:'searchbox.tpl.html', position:'top-left'};
+    $scope.clickOnMarker = function(doctor) {
+        $http.get('/api/doctors/'+doctor._id).success(function(data, status) {
+            console.log(data);
+            $scope.doctorDetail = data;
+        });
+    };
+
+    $scope.searchbox = { template:'searchbox.tpl.html', position:'top-left'};
+
   });
