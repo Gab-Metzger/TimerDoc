16ec4d9e925991604af3d8568735bf4d41767fdb
Gab-Metzger <gab.metzger@gmail.com>
2014-11-22 18:08:01 +0100

    Improve Main section

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,169 +1,135 @@
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('timerDocFullstackApp')
-  .controller('MainCtrl', ['$scope', '$http', 'socket', 'GoogleMapApi'.ns(), 'geolocation', 'doctorService', function ($scope, $http, socket, GoogleMapApi, geolocation, doctorService) {
+    angular
+        .module('timerDocFullstackApp')
+        .controller('MainCtrl', MainCtrl);
 
-  activate();
+    MainCtrl.$inject = ['socket', 'GoogleMapApi'.ns(), 'geolocation', 'doctorService'];
 
-  function activate() {
-      return getDoctors().then(function() {
-          console.log('Activated Main View');
-      });
-  }
+    function MainCtrl(socket, GoogleMapApi, geolocation, doctorService) {
 
-  function getDoctors() {
-      return doctorService.getDoctors()
-          .then(function(data) {
-              for (var i = 0; i < data.length; i++) {
-                  if (data[i].state === 'close') {
-                      data[i].time = 'Fermé';
-                  }
-                  else if (data[i].state === 'appointment') {
-                      data[i].time = 'RdV';
-                  }
-                  else if (data[i].state === 'nothing') {
-                      data[i].time = 'N/R';
-                  }
-                  else {
-                      data[i].time = (data[i].nbPatient * data[i].averageTime) + ' mn';
-                  }
-              }
-              $scope.doctors = data;
-              return $scope.doctors;
-          });
-  }
+        /*jshint validthis: true */
+        var vm = this;
+        vm.doctors = [];
+        vm.clickOnMarker = clickOnMarker;
 
-    socket.syncUpdates('doctor', $scope.doctors);
+        activate();
 
-    $scope.chooseLabelColor = function(doctor) {
-
-        if(doctor.state === 'close' || doctor.state === 'nothing') {
-            return 'marker-labels-grey';
-        }
-        else if (doctor.state === 'appointment') {
-            return 'marker-labels-blue';
-        }
-        else {
-            if ((doctor.nbPatient * doctor.averageTime) >= 60) {
-                return 'marker-labels-red';
-            }
-            else {
-                return 'marker-labels-green';
-            }
+        function activate() {
+            return getDoctors().then(function () {
+                console.log('Activated Main View');
+            });
         }
 
-    };
+        function getDoctors() {
+            return doctorService.getDoctors()
+                .then(function (data) {
+                    vm.doctors = data;
+                    return vm.doctors;
+                });
+        }
 
-    $scope.clickOnMarker = function(doctor) {
-        $http.get('/api/doctors/'+doctor._id).success(function(data) {
-            if (data.state === 'close') {
-                data.time = 'Fermé';
-            }
-            else if (data.state === 'appointment') {
-                data.time = 'RdV';
-            }
-            else if (data.state === 'nothing') {
-                data.time = 'N/R';
-            }
-            else {
-                data.time = (data.nbPatient * data.averageTime) + ' mn';
-            }
-            $scope.doctorDetail = data;
-        });
-    };
+        socket.syncUpdates('doctor', vm.doctors);
 
-
-    GoogleMapApi.then(function(maps) {
-        maps.visualRefresh = true;
-        $scope.defaultBounds = new google.maps.LatLngBounds(
-            new google.maps.LatLng(48.553493, 7.749963),
-            new google.maps.LatLng(48.550886, 7.735511));
-
-        $scope.map.bounds = {};
-
-        $scope.map.bounds = {
-            northeast: {
-                latitude: $scope.defaultBounds.getNorthEast().lat(),
-                longitude: $scope.defaultBounds.getNorthEast().lng()
-            },
-            southwest: {
-                latitude: $scope.defaultBounds.getSouthWest().lat(),
-                longitude: -$scope.defaultBounds.getSouthWest().lng()
-
-            }
+        function clickOnMarker(doctor) {
+            doctorService.getDoctor(doctor._id).then(function (data) {
+                vm.doctorDetail = data;
+            })
         };
 
-        geolocation.getLocation().then(function(data){
-            $scope.map.center = {latitude:data.coords.latitude, longitude:data.coords.longitude};
-            $scope.map.zoom = 14;
-        }, function() {
-            $scope.map.center = {latitude:46.71109, longitude:1.7191036};
-            $scope.map.zoom = 5;
+        GoogleMapApi.then(function (maps) {
+            maps.visualRefresh = true;
+            vm.defaultBounds = new google.maps.LatLngBounds(
+                new google.maps.LatLng(48.553493, 7.749963),
+                new google.maps.LatLng(48.550886, 7.735511));
+
+            vm.map.bounds = {};
+
+            vm.map.bounds = {
+                northeast: {
+                    latitude: vm.defaultBounds.getNorthEast().lat(),
+                    longitude: vm.defaultBounds.getNorthEast().lng()
+                },
+                southwest: {
+                    latitude: vm.defaultBounds.getSouthWest().lat(),
+                    longitude: -vm.defaultBounds.getSouthWest().lng()
+
+                }
+            };
+
+            geolocation.getLocation().then(function (data) {
+                vm.map.center = {latitude: data.coords.latitude, longitude: data.coords.longitude};
+                vm.map.zoom = 14;
+            }, function () {
+                vm.map.center = {latitude: 46.71109, longitude: 1.7191036};
+                vm.map.zoom = 5;
+            });
+
+            vm.searchbox.options.bounds = {};
+            vm.searchbox.options.bounds = new google.maps.LatLngBounds(vm.defaultBounds.getNorthEast(), vm.defaultBounds.getSouthWest());
         });
 
-        $scope.searchbox.options.bounds = {};
-        $scope.searchbox.options.bounds = new google.maps.LatLngBounds($scope.defaultBounds.getNorthEast(), $scope.defaultBounds.getSouthWest());
-    });
+        angular.extend(vm, {
+            selected: {
+                options: {
+                    visible: false
 
-    angular.extend($scope, {
-        selected: {
-            options: {
-                visible:false
-
+                },
+                templateparameter: {}
             },
-            templateparameter: {}
-        },
-        map: {
-            control: {},
-            dragging: false,
-            bounds: {},
-            idkey: 'place_id',
-            events: {
-                idle: function (map) {
-                    var bounds = map.getBounds();
-                    var ne = bounds.getNorthEast(); // LatLng of the north-east corner
-                    //console.log("ne bounds " + ne.lat() + ", " + ne.lng());
-                    var sw = bounds.getSouthWest(); // LatLng of the south-west corder
-                    //console.log("sw bounds " + sw.lat() + ", " + sw.lng());
+            map: {
+                control: {},
+                dragging: false,
+                bounds: {},
+                idkey: 'place_id',
+                events: {
+                    idle: function (map) {
+                        var bounds = map.getBounds();
+                        var ne = bounds.getNorthEast(); // LatLng of the north-east corner
+                        //console.log("ne bounds " + ne.lat() + ", " + ne.lng());
+                        var sw = bounds.getSouthWest(); // LatLng of the south-west corder
+                        //console.log("sw bounds " + sw.lat() + ", " + sw.lng());
+                    }
+
                 }
-
-            }
-        },
-        searchbox: {
-            template:'searchbox.tpl.html',
-            position:'top-left',
-            options: {
-                bounds: {}
             },
-            //parentdiv:'searchBoxParent',
-            events: {
-                places_changed: function (searchBox) {
-                    var places = searchBox.getPlaces()
-                    if (places.length == 0) {
-                        return;
-                    }
-                    // For each place, get the icon, place name, and location.
-                    var bounds = new google.maps.LatLngBounds();
-                    for (var i = 0, place; place = places[i]; i++) {
-                        bounds.extend(place.geometry.location);
-                    }
-
-                    $scope.map.bounds = {
-                        northeast: {
-                            latitude: bounds.getNorthEast().lat()+0.0025822,
-                            longitude: bounds.getNorthEast().lng()+0.0187469
-                        },
-                        southwest: {
-                            latitude: bounds.getSouthWest().lat()-0.0015458,
-                            longitude: bounds.getSouthWest().lng()-0.0063191
+            searchbox: {
+                template: 'searchbox.tpl.html',
+                position: 'top-left',
+                options: {
+                    bounds: {}
+                },
+                //parentdiv:'searchBoxParent',
+                events: {
+                    places_changed: function (searchBox) {
+                        var places = searchBox.getPlaces();
+                        if (places.length == 0) {
+                            return;
                         }
-                    };
+                        // For each place, get the icon, place name, and location.
+                        var bounds = new google.maps.LatLngBounds();
+                        for (var i = 0, place; place = places[i]; i++) {
+                            bounds.extend(place.geometry.location);
+                        }
 
+                        vm.map.bounds = {
+                            northeast: {
+                                latitude: bounds.getNorthEast().lat() + 0.0025822,
+                                longitude: bounds.getNorthEast().lng() + 0.0187469
+                            },
+                            southwest: {
+                                latitude: bounds.getSouthWest().lat() - 0.0015458,
+                                longitude: bounds.getSouthWest().lng() - 0.0063191
+                            }
+                        };
+
+                    }
                 }
+
+
             }
+        });
+    };
+})();
 
-
-        }
-    });
-
-  }]);
--- client/components/doctor/doctor.service.js
+++ client/components/doctor/doctor.service.js
@@ -9,7 +9,8 @@ doctorService.$inject = ['$http'];
 
 function doctorService($http) {
     return {
-        getDoctors: getDoctors
+        getDoctors: getDoctors,
+        getDoctor: getDoctor
     };
 
     function getDoctors() {
@@ -18,11 +19,65 @@ function doctorService($http) {
             .catch(getDoctorsFailed);
 
         function getDoctorsComplete(response) {
-            return response.data;
+            var data = response.data;
+            for (var i = 0; i < data.length; i++) {
+                sortState(data[i]);
+                styleState(data[i]);
+            }
+            return data;
         }
 
         function getDoctorsFailed(error) {
-            console.log('XHR Failed for getAvengers.' + error.data);
+            console.log('XHR Failed for getDoctors.' + error.data);
+        }
+    }
+
+    function getDoctor(id) {
+        return $http.get('/api/doctors/'+id)
+            .then(getDoctorComplete)
+            .catch(getDoctorFailed);
+
+        function getDoctorComplete(response) {
+            var data = response.data;
+            sortState(data);
+            return data;
+        }
+
+        function getDoctorFailed(error) {
+            console.log('XHR Failed for getDoctor.' + error.data);
+        }
+    }
+
+    function sortState(data) {
+        data.style = {};
+        if (data.state === 'close') {
+            data.style.label = 'Fermé';
+        }
+        else if (data.state === 'appointment') {
+            data.style.label = 'RdV';
+        }
+        else if (data.state === 'nothing') {
+            data.style.label = 'N/R';
+        }
+        else {
+            data.style.label = (data.nbPatient * data.averageTime) + ' mn';
+        }
+    }
+
+    function styleState(data) {
+        if (data.state === 'close' || data.state === 'nothing') {
+            data.style.color = 'marker-labels-grey';
+        }
+        else if (data.state === 'appointment') {
+            data.style.color = 'marker-labels-blue';
+        }
+        else {
+            if ((data.nbPatient * data.averageTime) >= 60) {
+                data.style.color = 'marker-labels-red';
+            }
+            else {
+                data.style.color = 'marker-labels-green';
+            }
         }
     }
 }
